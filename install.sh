0a6260b1f202f22e9746e0a51ebdf847c7f6c308
Robert Böhnke <robb@robb.is>
2013-03-23 21:33:54 +0100

    Adding install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p "$HOME/Library/Developer/Xcode/Templates/File Templates"
+ln -sf "$PWD/File Templates" "$HOME/Library/Developer/Xcode/Templates/File Templates/Nearest Neighbors"
+
