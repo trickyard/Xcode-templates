db17db860a0b6d30c44fcc2c644016a740a5ba5b
Robert Böhnke <robb@robb.is>
2013-03-24 12:37:16 +0100

    Adding Code Snippets

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-mkdir -p "$HOME/Library/Developer/Xcode/Templates/File Templates"
-ln -sf "$PWD/File Templates" "$HOME/Library/Developer/Xcode/Templates/File Templates/Nearest Neighbors"
+if [ ! -e "$HOME/Library/Developer/Xcode/Templates/File Templates/Nearest Neighbors" ];
+then
+    echo "Linking File Templates"
+    mkdir -p "$HOME/Library/Developer/Xcode/Templates/File Templates"
+    ln -s "$PWD/File Templates" "$HOME/Library/Developer/Xcode/Templates/File Templates/Nearest Neighbors"
+fi
 
+if [ ! -e "$HOME/Library/Developer/Xcode/UserData/CodeSnippets" ];
+then
+    echo "Linking Code Snippets"
+    mkdir -p "$HOME/Library/Developer/Xcode/UserData"
+    ln -s "$PWD/Code Snippets" "$HOME/Library/Developer/Xcode/UserData/CodeSnippets"
+fi
